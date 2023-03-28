d59cc0571365169478266230bc90a938c997ca48
Danis Gareev <gareev@bb.microcomp.sk>
2023-03-28 15:42:51 +0200

    updates

--- src/main/java/com/example/demo/controller/DeviceController.java
+++ src/main/java/com/example/demo/controller/DeviceController.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping(value = "device")
+@RequestMapping(value = "/")
 public class DeviceController {
     private DeviceService deviceService;
 
